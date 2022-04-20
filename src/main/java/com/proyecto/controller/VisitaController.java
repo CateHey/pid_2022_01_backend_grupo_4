ff1ee66963d88459236fff9d8456fb9bb4be5166
davidhom13 <davidhugo.ortiz@gmail.com>
2022-04-20 17:51:25 -0500

    Registro de Visita

---
+++ src/main/java/com/proyecto/controller/VisitaController.java
@@ -0,0 +1,55 @@
+package com.proyecto.controller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.proyecto.entidad.Visita;
+import com.proyecto.service.VisitaService;
+import com.proyecto.util.AppSettings;
+
+@RestController
+@RequestMapping("/rest/visita")
+@CrossOrigin(origins =AppSettings.URL_CROSS_ORIGIN)
+
+public class VisitaController {
+	
+	@Autowired
+	private VisitaService visitaService;
+	
+	@GetMapping
+	@ResponseBody
+	public ResponseEntity<List<Visita>> listaVisita(){
+		List<Visita> lst = visitaService.listaVisita();
+		return ResponseEntity.ok(lst);
+	}
+	
+	@PostMapping
+	@ResponseBody
+	public ResponseEntity<Map<String, Object>> insertaVisita(@RequestBody Visita obj){
+		Map<String, Object> salida = new HashMap<>();
+		try {
+			Visita objSalida = visitaService.insertaActualizaVisita(obj);
+			if(objSalida == null) {
+				salida.put("mensaje", com.proyecto.util.Constantes.MENSAJE_REG_ERROR);
+			}else {
+				salida.put("mensaje", com.proyecto.util.Constantes.MENSAJE_REG_EXITOSO);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			salida.put("mensaje", com.proyecto.util.Constantes.MENSAJE_REG_ERROR);
+		}
+		return ResponseEntity.ok(salida);
+	}
+
+}
---
+++ src/main/java/com/proyecto/repository/VisitaRepository.java
@@ -0,0 +1,9 @@
+package com.proyecto.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.proyecto.entidad.Visita;
+
+public interface VisitaRepository extends JpaRepository<Visita, Integer>{
+
+}
