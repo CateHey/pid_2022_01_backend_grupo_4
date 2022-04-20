c275510f73eeaaef9eb28301a347ade2a5696543
CateHey <83050179+CateHey@users.noreply.github.com>
2022-04-20 12:11:55 -0500

    Delete src/main/java/pid/proyecto/repository directory

--- src/main/java/pid/proyecto/repository/MascotaRepository.java
+++
@@ -1,9 +0,0 @@
-package pid.proyecto.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import pid.proyecto.entidad.Mascota;
-
-public interface MascotaRepository extends JpaRepository<Mascota, Integer> {
-
-}
