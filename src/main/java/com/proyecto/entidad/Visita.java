7358a549c97aaae3dbdb635bca35e1fdfd39f937
davidhom13 <davidhugo.ortiz@gmail.com>
2022-04-23 09:17:26 -0500

    Cambio de String a Date en FechaVisita

--- src/main/java/com/proyecto/entidad/Visita.java
+++ src/main/java/com/proyecto/entidad/Visita.java
@@ -1,10 +1,16 @@
 package com.proyecto.entidad;
 
+import java.util.Date;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -18,10 +24,11 @@ public class Visita {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int cod_vis;
-	private String fech_vis;
-	private String hor_vis;
-	private String tipo_vis;
+	@Temporal(TemporalType.TIMESTAMP)
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd hh:mm:ss")
+	private Date fech_vis;
+	private String nom_vis;
+	private String ape_vis;
 	private int cod_dep;
-	private int cod_usu;
 
 }
