80834dc760024abcb045f50abcfd531dcdddd09a
CateHey <catyvaras19@gmail.com>
2022-05-24 15:21:29 -0500

    Backend para busqueda de visitante

--- src/main/java/com/proyecto/repository/VisitaRegRepository.java
+++ src/main/java/com/proyecto/repository/VisitaRegRepository.java
@@ -12,7 +12,5 @@ public interface VisitaRegRepository extends JpaRepository<VisitaReg, Integer>{
 
     @Query("SELECT r FROM VisitaReg r JOIN Visita v ON v.cod_vis = r.visita " +
             "WHERE (?1 is '' or v.nom_vis LIKE ?1) and (?2 is '' or v.dni_vis LIKE ?2) and (?3 is -1 or r.estado_visreg = ?3)")
-
-
     public List<VisitaReg> listaVisitaPorNombreDniEstado(String nom_vis, String dni_vis, int estado_visreg);
 }
--- src/main/java/com/proyecto/repository/VisitaRepository.java
+++ src/main/java/com/proyecto/repository/VisitaRepository.java
@@ -1,9 +1,15 @@
 package com.proyecto.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.proyecto.entidad.Visita;
 
 public interface VisitaRepository extends JpaRepository<Visita, Integer>{
+	
+	 @Query("SELECT v FROM Visita v WHERE (?1 is '' or v.dni_vis LIKE ?1)")
+	 public List<Visita> listaVisitaPorDni(String dni_vis);
 
 }
