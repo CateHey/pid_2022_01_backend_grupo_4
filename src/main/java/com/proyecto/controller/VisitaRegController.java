8a2e329cf6dc64261becb4c13359b7c388d68156
leoleo927 <lenfer927@gmail.com>
2022-05-23 21:33:08 -0500

    arreglar consulta query

--- src/main/java/com/proyecto/controller/VisitaRegController.java
+++ src/main/java/com/proyecto/controller/VisitaRegController.java
@@ -7,13 +7,8 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.*;
 
 import com.proyecto.entidad.VisitaReg;
 import com.proyecto.service.VisitaRegService;
@@ -46,6 +41,29 @@ public class VisitaRegController {
 				salida.put("mensaje", com.proyecto.util.Constantes.MENSAJE_REG_ERROR);
 			}else {
 				salida.put("mensaje", com.proyecto.util.Constantes.MENSAJE_REG_EXITOSO);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			salida.put("mensaje", com.proyecto.util.Constantes.MENSAJE_REG_ERROR);
+		}
+		return ResponseEntity.ok(salida);
+	}
+
+
+	@GetMapping("/listaVisitaConParametros")
+	@ResponseBody
+	public ResponseEntity<Map<String, Object>> listaVisitaPorDNINombreEstado(
+			@RequestParam(name = "nombre", required = false, defaultValue = "") String nom_vis,
+			@RequestParam(name = "dni", required = false, defaultValue = "") String dni_vis,
+			@RequestParam(name = "estado", required = true, defaultValue = "1") int estado_visreg) {
+		Map<String, Object> salida = new HashMap<>();
+		try {
+			List<VisitaReg> lista = visitaregService.listaVisitaPorNombreDniEstado(nom_vis, dni_vis, estado_visreg);
+			if (CollectionUtils.isEmpty(lista)) {
+				salida.put("mensaje", "No existen datos para mostrar");
+			}else {
+				salida.put("lista", lista);
+				salida.put("mensaje", "Existen " + lista.size() + " elementos para mostrar");
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
